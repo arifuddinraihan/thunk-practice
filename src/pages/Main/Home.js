b2f037b061cfea510a723ff84468cf8f7050201d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-07 22:30:33 +0600

    added filtes wise conditional redner and reducer codes for filters

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
-import { toggleBrand } from "../../redux/actions/filterAction";
+import { toggleBrand, toggleStock } from "../../redux/actions/filterAction";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const filters = useSelector((state) => state.filter.filters)
+  const { brands, stock } = filters;
+  // console.log(brands)
   const dispatch = useDispatch();
   useEffect(() => {
     fetch("http://localhost:5000/products")
@@ -18,14 +21,15 @@ const Home = () => {
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          onClick={() => dispatch(toggleStock())}
+          className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null}`}
         >
           In Stock
         </button>
-        <button onClick={() => dispatch(toggleBrand("amd"))} className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={() => dispatch(toggleBrand("amd"))} className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("amd") ? activeClass : null}`}>
           AMD
         </button>
-        <button onClick={() => dispatch(toggleBrand("intel"))} className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={() => dispatch(toggleBrand("intel"))} className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("intel") ? activeClass : null}`}>
           Intel
         </button>
       </div>
--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -5,21 +5,39 @@ export const initialState = {
         brands: [],
         stock: false,
     },
-    keywords: ""
+    keywords: "",
 }
 
 export const filterReducer = (state = initialState, action) => {
     switch (action.type) {
         case TOGGLE_BRANDS:
+            if (!state.filters.brands.includes(action.payload)) {
+                return {
+                    ...state,
+                    filters: {
+                        ...state.filters,
+                        brands: [...state.filters.brands, action.payload],
+                    }
+                };
+            } else {
+                return {
+                    ...state,
+                    filters: {
+                        ...state.filters,
+                        brands: 
+                        state.filters.brands
+                        .filter(brand => brand !== action.payload),
+                    }
+                };
+            }
+        case TOGGLE_STOCK:
             return {
                 ...state,
                 filters: {
                     ...state.filters,
-                    brands: [...state.filters.brands, action.payload],
-                }
+                    stock : !state.filters.stock,
+                },
             };
-        case TOGGLE_STOCK:
-            return {};
         default:
             return state;
     }
