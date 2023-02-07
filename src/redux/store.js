b9b351bb2aca0c3bcf315fa622060582b370dda3
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-08 00:22:44 +0600

    added cartCounter as middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
+import cartCounter from "./middlewares/cartCounter";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+const store = createStore(rootReducer,
+    composeWithDevTools(applyMiddleware(cartCounter)));
 
 export default store;
