f399cacb567613d0f98e113bac32fd1db023fcb3
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-09 13:39:48 +0600

    added add product action and dispatch in product list page

--- src/pages/Dashboard/AddProduct.js
+++ src/pages/Dashboard/AddProduct.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../redux/actions/productAction";
 
 const AddProduct = () => {
   const { register, handleSubmit } = useForm();
-
+  const dispatch = useDispatch();
   const submit = (data) => {
     const product = {
       model: data.model,
@@ -20,6 +22,7 @@ const AddProduct = () => {
     };
 
     console.log(product);
+    dispatch(addProduct(product));
   };
 
   return (
